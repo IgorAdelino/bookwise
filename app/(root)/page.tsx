ce7315a050b5cea8bf047f5797043ac9312164bc
Igor Adelino <igoradelino@gmail.com>
2025-02-10 16:08:06 -0300

    feat: home page config

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { BookList } from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <>
+    <BookOverview/>
+    <BookList/>
+    </>
+  );
+}
+
+export default Home
