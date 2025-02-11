3bd19f7505abb71c525da97673bbb1df609f98d2
Igor Adelino <igoradelino@gmail.com>
2025-02-11 13:18:16 -0300

    fix: styles

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -12,21 +12,26 @@ const BookOverview = ({title, author, genre, rating, totalCopies, availableCopie
           {title}
         </h1>
         <div className='book-info'>
-            <p>By <span className='font-semibold text-light-200'>{author}</span></p>
-            <p>Category{" "}<span className='font-semibold text-light-200'>{genre}</span></p>
+            <p>
+              By <span className='font-semibold text-light-200'>{author}</span>
+            </p>
+            <p>
+              Category{" "}<span className='font-semibold text-light-200'>{genre}</span>
+              </p>
+          <div className='flex flex-row gap-1'>
+            <Image src="/icons/star.svg" alt="star" width={22} height={22}/>
+            <p>{rating}</p>
+          </div>
         </div>
-        <div className='flex flex-row gap-1'>
-          <Image src="/icons/star.svg" alt="star" width={22} height={22}/>
-          <p>{rating}</p>
-        </div>
-        <div className='book-copies'>
-        <p>
-          Total Books: <span>{totalCopies}</span>
-        </p>
 
-        <p>
-          Available Books: <span>{availableCopies}</span>
-        </p>
+        <div className='book-copies'>
+          <p>
+            Total Books: <span>{totalCopies}</span>
+          </p>
+
+          <p>
+            Available Books: <span>{availableCopies}</span>
+          </p>
         </div>
         <p className='book-description'>
           {description}
@@ -36,7 +41,8 @@ const BookOverview = ({title, author, genre, rating, totalCopies, availableCopie
           <p className='font-bebas-neue text-xl text-dark-100'>Borrow</p>
         </Button>
       </div>
-      <div className='relative flex flex-2 justify-center'>
+
+      <div className='relative flex flex-1 justify-center'>
         <div className='relative'>
           <BookCover variant="wide" className="z-10" coverColor={color} coverImage={coverImage}/>
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
     
     <header className='my-10 flex justify-between gap-5'>
       <Link href="/">
-      <Image src="/icons/logo.svg" alt="logo" width={100} height={100}/>
+      <Image src="/icons/logo.svg" alt="logo" width={40} height={40}/>
       </Link>
       <ul className='flex flex-row items-center gap-8'>
         <li>
